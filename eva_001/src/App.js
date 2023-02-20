76338d98d8bce67bdcc967815f2d2b84a4b70d20
pknalex <pau20359@spengergasse.at>
2023-02-20 15:00:00 +0100

    First implementations of Frontend (HTML and CSS)

--- eva_001/src/App.js
+++ eva_001/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './App.css';
+
 
 export default class App extends Component {
     static displayName = App.name;
@@ -12,42 +14,24 @@ export default class App extends Component {
         this.populateWeatherData();
     }
 
-    static renderForecastsTable(forecasts) {
-        return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.date}>
-                            <td>{forecast.date}</td>
-                            <td>{forecast.temperatureC}</td>
-                            <td>{forecast.temperatureF}</td>
-                            <td>{forecast.summary}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
-
     render() {
-        let contents = this.state.loading
-            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            : App.renderForecastsTable(this.state.forecasts);
-
         return (
-            <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                {contents}
-            </div>
+            <body>
+                <div className="App">
+                    <header>EVA</header>
+                    <div id="container">
+                        <div id="content-container">
+                            <p>Hier steht viel Content spaeter und es wird den meisten Platz wegnehmen</p>
+                        </div>
+                        <footer class="flex-container">
+                            <div>Home</div>
+                            <div>Leaderboard</div>
+                            <div>Goals</div>
+                            <div>Profile</div>
+                        </footer>
+                    </div>
+                </div>
+            </body>
         );
     }
 
